3ed95058b12b4079bf550103a5ea663b2d014c80
morglod <morglod@gmail.com>
2018-03-17 07:36:35 +0300

    example app layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,35 +10,47 @@ import {
 class App extends React.Component {
     render() {
         return (
-            <div style={{ padding: '20px' }}>
-                <Alerts>
-                    <Alert
-                        type="info"
-                        text="asd"
-                        actions={[]}
-                        icon="dollar"
-                    />
-                    <Alert
-                        type="danger"
-                        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-                        actions={[]}
-                        icon="exclamation-circle"
-                    />
-                    <Alert
-                        type="warning"
-                        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-                    />
-                </Alerts>
-                <br />
-                <Button children="Primary" primary />
-                <Button children="Saving" info outline loading />
-                <Button children="Primary" primary icon="dollar" />
-                <Button children="Saving" info outline loading disabled />
-                <Button primary icon="dollar" />
-                <Button info outline >
-                    Show items <Badge children={100} />
-                </Button>
-                <br />
+            <div className="main-container">
+                <header className="header header-6">
+                    <div className="branding">
+                        <a href="https://github.com/Morglod/react-vmware-clarity" target="_blank">
+                            <span className="title">React VMWare Clarity</span>
+                        </a>
+                    </div>
+                </header>
+                <nav className="subnav" />
+                <div className="content-container">
+                    <div className="content-area">
+                        <Alerts>
+                            <Alert
+                                type="info"
+                                text="asd"
+                                actions={[]}
+                                icon="dollar"
+                            />
+                            <Alert
+                                type="danger"
+                                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                                actions={[]}
+                                icon="exclamation-circle"
+                            />
+                            <Alert
+                                type="warning"
+                                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                            />
+                        </Alerts>
+                        <br />
+                        <Button children="Primary" primary />
+                        <Button children="Saving" info outline loading />
+                        <Button children="Primary" primary icon="dollar" />
+                        <Button children="Saving" info outline loading disabled />
+                        <Button primary icon="dollar" />
+                        <Button info outline >
+                            Show items <Badge children={100} />
+                        </Button>
+                    </div>
+                    <nav className="sidenav" />
+                </div>
             </div>
         );
     }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import '@clr/icons/clr-icons.min.css';
 import '@webcomponents/custom-elements/custom-elements.min.js';
-// import '@clr/icons/clr-icons.min.js';
 import '@clr/icons';
 import '@clr/ui/src/main.scss';
+// import '@clr/ui/src/dark-theme.scss';
 
 export const A = 123;
 

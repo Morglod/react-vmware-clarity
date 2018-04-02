08157904b2c0f2c0a378ba3616b8a8e53a72fc30
morglod <morglod@gmail.com>
2018-04-02 19:14:53 +0300

    wip on dropdowns

---
+++ src/components/dropdowns.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export type DropDownProps = {
+    button: React.ReactElement<{ onClick?: any }>,
+    children
+}
+
+export class DropDown extends React.PureComponent<DropDownProps> {
+    render() {
+
+    }
+}
